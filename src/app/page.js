7f8b7add2514c5166555fe9c8f43199fd94b65db
Naeem240 <haidermdnaeem@gmail.com>
2025-09-16 19:43:39 +0600

    [ADDED]: Primary and secondary color set up and remove unnecessary code from page.js and global.css

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>We are working</h1>
+    <div>
+    <h1 className="text-primary">This is Primary color</h1>
+    <h1 className="text-secondary">This is secondary color</h1>
+
+    </div>
+    
   );
 }
