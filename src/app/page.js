b991f1afca90c54b3c4f718e54dbbcbbe582a3c7
muhammad-adnan <mohammadadnanwasti@gmail.com>
2025-09-17 19:42:20 +0600

    Terms and Conditon page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <h2>This a test line from foysal</h2>
       <h1 className="text-primary">This is Primary color</h1>
       <h1 className="text-secondary">This is secondary color</h1>
+  <Link href='/Products' className="btn ">Terms and Condition</Link>
     </div>
   );
 }
