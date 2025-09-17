0f1607c1f39dae38adccba43685a8ec69658f47f
rafsan102 <ahmedrafsan101@gmail.com>
2025-09-18 01:47:04 +0600

    added banner sectionin homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import HomePage from "@/components/HomePage";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,7 +8,8 @@ export default function Home() {
       <h2>This a test line from foysal</h2>
       <h1 className="text-primary">This is Primary color</h1>
       <h1 className="text-secondary">This is secondary color</h1>
-  <Link href='/Products' className="btn ">Terms and Condition</Link>
+      <Link href='/Products' className="btn ">Terms and Condition</Link>
+      <HomePage></HomePage>
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,11 +5,13 @@ import TrustedCompanies from './sections/TrustedCompanies';
 import CareerTools from './sections/CareerTools';
 import SuccessStories from './sections/SuccessStories';
 import CallToAction from './sections/CallToAction';
+import Banner from './sections/Banner';
 
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       {/* Import and render all sections */}
+      <Banner></Banner>
       <ExploreCareerPaths />
       <FeaturedJobs />
       <TrustedCompanies />
