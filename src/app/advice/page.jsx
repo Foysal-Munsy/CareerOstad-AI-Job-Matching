0c7a8634006ad3bc68ad7669e836509618f68ea5
MohammedAmir322 <mohammedamir5949@gmail.com>
2025-09-19 00:07:55 +0600

    [added]: added career advice route and component

---
+++ src/app/advice/page.jsx
@@ -0,0 +1,12 @@
+import CareerAdvice from '@/components/careerAdvice/CareerAdvice';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <CareerAdvice></CareerAdvice>
+        </div>
+    );
+};
+
+export default page;
