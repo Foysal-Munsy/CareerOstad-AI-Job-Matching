e1942ff8e337e66228b05ef546916c3b29a24e68
rafsan102 <ahmedrafsan101@gmail.com>
2025-09-18 20:24:32 +0600

    update homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -14,7 +14,6 @@ const HomePage = () => {
     <div className="min-h-screen bg-gray-50">
       {/* Import and render all sections */}
       <Banner></Banner>
-
       <KeyHighlights />
       <HowItWorks />
       <ExploreCareerPaths />
