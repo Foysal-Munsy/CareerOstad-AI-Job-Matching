fef56a69eda485db4a642b6a9cf0c57e6e7a3265
Foysal-Munsy <bdfoysal555@gmail.com>
2025-10-05 23:02:45 +0600

    [added] basic stripe payment system.

--- src/components/stripePayment/StripePayment.jsx
+++ src/components/stripePayment/StripePayment.jsx
@@ -1,4 +1,13 @@
+"use client";
 import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import convertToSubcurrency from "@/lib/convertToSubcurrency";
+import CheckoutPage from "./CheckoutPage";
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined");
+}
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function StripePayment() {
   const amount = 29.99;
@@ -13,16 +22,16 @@ export default function StripePayment() {
           </h2>
         </div>
 
-        {/* <Elements
-        stripe={stripePromise}
-        options={{
-          mode: "payment",
-          amount: convertToSubcurrency(amount),
-          currency: "usd",
-        }}
-      >
-        <CheckoutPage amount={amount} />
-      </Elements> */}
+        <Elements
+          stripe={stripePromise}
+          options={{
+            mode: "payment",
+            amount: convertToSubcurrency(amount),
+            currency: "usd",
+          }}
+        >
+          <CheckoutPage amount={amount} />
+        </Elements>
       </div>
     </div>
   );
