dd5fdd40ebf479641a2ca481d9fb450bf73711b1
rafsan102 <ahmedrafsan101@gmail.com>
2025-10-06 23:12:36 +0600

    [update] update functionality in both matching page

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,28 +1,26 @@
-import { MongoClient, ServerApiVersion } from "mongodb"
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 export const collectionNamesObj = {
-    userCollection: "sample-user",
-    jobsCollection: "jobs",
-    adviceCollection: "advice",
-    applicationsCollection: "applications",
-    messagesCollection: "messages",
-    savedJobsCollection: "saved-jobs",
-}
-export default function dbConnect(collectionName) {
-    const uri = process.env.MONGO_URI;
-    const client = new MongoClient(uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        },
-        tls: true,
-        tlsAllowInvalidCertificates: true,
-        tlsAllowInvalidHostnames: true,
-        connectTimeoutMS: 30000,
-        socketTimeoutMS: 30000,
-        maxPoolSize: 10,
-        retryWrites: true
-    });
-    return client.db(process.env.DB_NAME).collection(collectionName);
+  userCollection: 'sample-user',
+  jobsCollection: 'jobs',
+  adviceCollection: 'advice',
+  applicationsCollection: 'applications',
+  messagesCollection: 'messages',
+  savedJobsCollection: 'saved-jobs',
+};
+
+export default async function dbConnect(collectionName) {
+  if (!client.topology?.isConnected()) {
+    await client.connect();
+  }
+  return client.db(process.env.DB_NAME).collection(collectionName);
 }
